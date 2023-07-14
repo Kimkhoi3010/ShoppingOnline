64a1e2c722a9b4ac6aa7ccbc7d5ad297bf984b4b
Kimkhoi3010 <kkimkhoi3010@gmail.com>
2023-07-14 12:55:32 +0700

    init project

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.govzhu8.mongodb.net',
+    DB_USER: 'kkimkhoi3010',
+    DB_PASS: 'Kimkhoipro.123',
+    DB_DATABASE: 'shoppingonline',
+    EMAIL_USER: 'kkimkhoi3010@gmail.com',
+    EMAIL_PASS: 'vifwwinonuyqmpvs',
+    JWT_SECRET: '<jwt_secret>',
+    JWT_EXPIRES: '31536000000', // in milliseconds
+  };
+  module.exports = MyConstants;
